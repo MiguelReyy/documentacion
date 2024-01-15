94a1258ed2035e8497999e0529cdbae79b291f66
Alvaro <asantant@myuax.com>
2024-01-15 22:15:49 +0100

    .

--- POO/poo_1.py
+++ POO/poo_1.py
@@ -1,20 +1,27 @@
 class Coche():
 
-    # 4 propiedades
-    largo_chasis = 250
-    ancho_chasis = 120
-    ruedas = 4
-    encendido = False
+    def __init__(self):
+        # 4 propiedades
+        self.largo_chasis = 250
+        self.ancho_chasis = 120
+        self.ruedas = 4
+        self.encendido = False
 
     # 2 comportamientos
-    def arrancar(self):
-        self.encendido = True   # self. significa "toyota.encendido = True"
-
-    def estado(self):
+    def arrancar(self,arrancamos):
+        self.encendido = arrancamos
+        
         if(self.encendido) == True:
             return "El coche está encendido"
         else:
             return "El coche está apagado"
+
+
+    def estado(self):
+       print("El coche tiene",self.ruedas,"ruedas. Un ancho de",self.ancho_chasis,
+             "y un largo de",self.largo_chasis,)
+
+
 
 # Instanciar una clase
 toyota = Coche()
@@ -22,5 +29,14 @@ toyota = Coche()
 print("El largo del coche es:",toyota.largo_chasis)
 print("El coche tiene",toyota.ruedas,"ruedas")
 
-toyota.arrancar()
-print(toyota.estado())
+print(toyota.arrancar(True))
+toyota.estado()
+
+print("\n------------A continuación creamos el segundo objeto------------"
+        + "\n")
+
+bmw = Coche()
+print(f"El largo del coche es:",bmw.largo_chasis)
+print("El coche tiene",bmw.ruedas,"ruedas")
+print(bmw.arrancar(False))
+bmw.estado()
