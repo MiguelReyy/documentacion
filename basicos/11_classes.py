03fe59e5cdeb9e7558d56039ac0151280dd06922
MiguelReyy <mreycar@myuax.com>
2024-01-17 22:03:47 +0100

    .

--- basicos/11_classes.py
+++ basicos/11_classes.py
@@ -17,7 +17,7 @@ print(MyEmptyPerson())
 class Person:
     def __init__(self, name, surname, alias="Sin alias"):
         self.full_name = f"{name} {surname} ({alias})"  # Propiedad pública
-        self.__name = name  # Propiedad privada
+        self.__name = name  # Propiedad privada por poner barrabaja, convenio
 
     def get_name(self):
         return self.__name
@@ -30,12 +30,3 @@ my_person = Person("Brais", "Moure")
 print(my_person.full_name)
 print(my_person.get_name())
 my_person.walk()
-
-my_other_person = Person("Brais", "Moure", "MoureDev")
-print(my_other_person.full_name)
-my_other_person.walk()
-my_other_person.full_name = "Héctor de León (El loco de los perros)"
-print(my_other_person.full_name)
-
-my_other_person.full_name = 666
-print(my_other_person.full_name)
